f053df09d3111b820d4b001557c9776d402631c9
Saif-IIT-JU <57219416+Saif-IIT-JU@users.noreply.github.com>
2022-12-01 21:23:35 +0600

    Added c.cpp and input.txt

---
+++ C.cpp
@@ -0,0 +1,86 @@
+/*
+
+    author : s@if
+
+*/
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define 		NIL 			-1
+#define			INF 			1e9
+#define         EPS             1e-9
+#define         SAIF            main
+#define		 	fi	 		    first
+#define 		sec 			second
+#define 		MAX 			INT_MAX
+#define 		ll 			    long long
+#define 		PI 			    acos(-1.0)
+#define			MOD 			1000000007
+#define 		PLL 			pair<ll,ll>
+#define 		PII			    pair<int,int>
+#define 		ull 			unsigned long long
+#define 		For(i,a,b) 		for(int i=a;i<=(int)b;i++)
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+            tree_order_statistics_node_update> new_data_set;
+
+//*find_by_order(k)  gives the kth element;
+//order_of_key(item)   gives the index(number of element strictly less than item) of item;
+
+inline int in() {int x; scanf("%d", &x); return x; }
+bool Check(int N , int pos)	{	return (bool) (N & (1<<pos));}
+int Set(int N, int pos) {	return N = N | (1<<pos);}
+
+int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};    // King's move
+int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};
+int hx[]={-2,-2,-1,+1,+2,+2,-1,+1};    // Knight's move
+int hy[]={+1,-1,+2,+2,-1,+1,-2,-2};
+int dx[]={+1,-1,+0,+0};
+int dy[]={+0,+0,+1,-1};
+
+const int MAXN = (int)2e5+9;
+
+int dp[1009][1009][3], n;
+
+int solve(int i, int k, int d)
+{
+   // cout<<i<<" "<<k<<" "<<d<<endl;
+    if(k==1 || i==0 || i==n+1) return 1;
+    if(dp[i][k][d]!=-1) return dp[i][k][d];
+
+    int ans = 0;
+
+    if(d==1)
+    {
+        ans+=solve(i+1, k, d); ans%=MOD;
+        ans+=solve(i-1, k-1, 1-d); ans%=MOD;
+    }
+    else
+    {
+        ans+=solve(i-1, k, d); ans%=MOD;
+        ans+=solve(i+1, k-1, 1-d); ans%=MOD;
+    }
+
+    return dp[i][k][d] = ans;
+}
+
+int SAIF()
+{
+    ll i,j,k,l,m,p,q,x,y,u,v,w,r,tc,t;
+
+    cin>>tc; while(tc--)
+    {
+        cin>>n>>k;
+
+        memset(dp, -1, sizeof(dp));
+
+        cout<<solve(1,k,1)<<endl;
+    }
+
+    return 0;
+}
+
+// read the question correctly (is y a vowel? what are the exact constraints?)
+// look out for SPECIAL CASES (n=1?) and overflow (ll vs int?)
